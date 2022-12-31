5e08eaaae25b9910562c9a675c54bdbad0412de4
Juvet <juvettaj@gmail.com>
2022-12-31 13:49:39 +0100

    commit post disgn

--- ApiBilanClubDegre/Program.cs
+++ ApiBilanClubDegre/Program.cs
@@ -24,15 +24,15 @@ builder.Services.AddCors(options => options.AddPolicy("Cors", policy =>
 }));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+   // app.UseSwaggerUI();
 }
 app.UseCors("Cors");
 app.UseHttpsRedirection();
